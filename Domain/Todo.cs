0c0bf7768c5f553485b26c982859ce42a40ce660
acki-01 <zamf124@gmail.com>
2021-04-03 20:33:20 +0200

    Initial Skeleton, Todo Model, and DB connection

---
+++ Domain/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Comment { get; set; }
+        public int Category { get; set; }
+        public int Priority { get; set; }
+        public DateTime Created_At { get; set; }
+
+    }
+}
